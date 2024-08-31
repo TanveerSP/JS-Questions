b5bf04bca46a80207bef9fe23656050463cc5038
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-08-31 20:34:50 +0530

    Array Methods

--- Array/01_Basic.js
+++ Array/01_Basic.js
@@ -1,1 +1,8 @@
-console.log("Array's")
+let frutes = [];
+console.log("Befor Array => ", frutes);
+
+console.log(frutes.push("Apple", "Banana", "Chery", "Pinapple"));
+console.log("After Using Push Method => ", frutes);
+
+console.log(frutes.pop());
+console.log("After Using Pop Method => ", frutes);
