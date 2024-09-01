48f60c2bdfd901132b2b57d4f10ae2d9937c7ee3
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-01 12:38:39 +0530

    slice and splice method

---
+++ Array_Methods/03_Slice_and_Splice.js
@@ -0,0 +1,6 @@
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let slicedArray = num.slice(2, 7);
+console.log('Sliced Array =>', slicedArray);
+
+let animal = ["Tiger", "Loin", "Puma", "Panther", "Cat"]
+let splicedArray = animal.splice()
