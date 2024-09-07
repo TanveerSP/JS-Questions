314f68d612925bc6d7a8429b591e24a7b5b3d2f5
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-07 22:47:35 +0530

    practice of learded fun of array

--- Array_Methods/combinAllInOne.js
+++ Array_Methods/combinAllInOne.js
@@ -1,3 +1,4 @@
+/*
 let fruits = ["Apple","Banana","Chairy","DrigonFruit","Mango", "Orange"];
 
 // 1. push() - Add an element to the end of the array
@@ -37,3 +38,44 @@ console.log("Index of you Search Frute => ",findFruit);
 
 // Final state of the array
 console.log(fruits)
+
+*/
+
+let phones = ["Apple","Oppo","Google Pixel","Xiaomi","Vivo"];
+console.log("Oregnal Array => ",phones);
+
+// push "IQOO";
+phones.push("IQOO");
+console.log("After push Item in Org Arr => ", phones)
+
+// pop Item in phones
+let popItem = phones.pop()  // remove 'IQOO' in the array
+console.log("After pop Item in Org Arr => ",phones)
+console.log("popped Item in phone => ",popItem)
+
+// unshift Item "OnePluse"
+phones.unshift("OnePluse") // Add Item in Array first Index
+console.log("After unshift Item on first index on array",phones); 
+
+phones.shift();
+console.log("Remove first index Item in Array => ",phones);
+
+// ============ splice modify the real array =============== //
+let spliceItme = phones.splice(2,2, "realme","Honer"); // Remove array index Item, and replace with this Items "Honer","Realme"
+console.log("Removed Item Array => ",spliceItme);
+console.log("After Removed and Add new Item in Array => ",phones);
+
+// =============== slice is createting ============== //
+let sliceItme = phones.slice(3,5); // Create a subarray without modifying the original array
+console.log("slice Item in Array => ", sliceItme) 
+console.log("Oregnal Array => ",phones);
+
+let includesItem = phones.includes("Oppo")
+console.log("Check Item is Present or Not => ",includesItem);
+console.log("Oregnal Array => ", phones);
+
+let indexOfItem = phones.indexOf("Oppo");
+console.log("Show index no of you search in Array if present",indexOfItem);
+
+// Final State of array
+console.log(phones);
