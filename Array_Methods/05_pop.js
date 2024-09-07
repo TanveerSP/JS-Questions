981d1a3512973cbfa7cdb32ff6a2719f4c60efb2
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-07 14:26:03 +0530

    include and indexof

--- Array_Methods/05_pop.js
+++
@@ -1,7 +0,0 @@
-let nums = [1,2,4,5,6];
-console.log("Before pop function applay =>", nums);
-
-let numIsPop = nums.pop()
-
-console.log("pop function store pop num =>",numIsPop);
-console.log("After pop function applay =>",nums)
