33755911df954ef6ecf45de90caa4d7f2fd0356d
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-08 10:11:20 +0530

    practice of concat

---
+++ Array_Methods/06_concatinat_and_spred.js
@@ -0,0 +1,19 @@
+const group1 = ["Nike", "Adidas", "Puma"];
+const group2 = ["Spark", "WallLine", "Paregon"];
+
+const combainGrp = group1.concat(group2)
+console.log("Combain Two Array in One Array => ",combainGrp);
+
+const fruts = ["Apple","Mango","Oreange"];
+const numbers = [1,2,3];
+const cars = ["BMW","Mercedic","Ferari"]
+
+// fruts.push(numbers);
+// console.log("After Push number Arr in fruts Arr => ",fruts);
+
+let mixArr = fruts.concat(numbers,cars);
+console.log("After mix two array using concat => ",mixArr);
+
+
+
+
