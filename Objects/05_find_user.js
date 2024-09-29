f7f6d1acf7672efabf123fa70bbd6cb9f3099f40
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-29 19:46:26 +0530

    Question: Given an array of user objects, find the user with a specific email.

---
+++ Objects/05_find_user.js
@@ -0,0 +1,9 @@
+// Question: Given an array of user objects,
+// find the user with a specific email.
+
+const userEmail = [
+    { id: 1, email: "empAtMicrosoft@gmail.com" },
+    { id: 2, email: "empAtGoogle@gmail.com"},
+    { id: 3, email: "empAtAmazone@gmail.com"},
+    {id:4, email: "empAtNetflix@gmail.com"}
+]
