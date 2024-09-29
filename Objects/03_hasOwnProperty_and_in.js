11220fcf798827a888d074af4c69c7c970a1ebbe
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-29 11:09:10 +0530

    more practice

--- Objects/03_hasOwnProperty_and_in.js
+++ Objects/03_hasOwnProperty_and_in.js
@@ -5,10 +5,26 @@ const car = {
     model: "Camry",
 };
 
-// Using hasOwnProperty()
-console.log(car.hasOwnProperty('brand'));
-console.log(car.hasOwnProperty('year'));
+// // Using hasOwnProperty()
+// console.log(car.hasOwnProperty('brand'));
+// console.log(car.hasOwnProperty('year'));
 
-// using the 'in' operator
-console.log('model'in car);
-console.log('year'in car);
+// // using the 'in' operator
+// console.log('model'in car);
+// console.log('year'in car);
+
+
+const phone = {
+    brand: "Apple",
+    model: "iPhone 16 Max Pro",
+    year: 2024-25
+}
+console.log(phone.hasOwnProperty('year'));
+console.log(phone.hasOwnProperty('brand'));
+console.log(phone.hasOwnProperty('owner'));
+console.log(phone.hasOwnProperty('model'));
+
+console.log('year' in phone);
+console.log('brand' in phone);
+console.log('owner' in phone);
+console.log('model' in phone);
