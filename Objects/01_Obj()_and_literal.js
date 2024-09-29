ede4001e1b13f0c98d82786ff74488d74ce325dc
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-29 10:08:30 +0530

    i write code using new Object()

--- Objects/01_Obj()_and_literal.js
+++ Objects/01_Obj()_and_literal.js
@@ -1,26 +1,6 @@
-// Question 1: How do you create an object using a constructor function in JavaScript
-function User(name,age){
-    this.name = name;
-    this.age = age;
-};
-const user1 = new User("Tanveer", 21);
+// Create an Object using new Object()
+const user1 = new Object();
+user1.id = 1;
+user1.name = "Adam";
+
 console.log(user1);
-
-// Question 2: How can you add a method to an existing object?
-const person = {
-    name: "Tanveer"
-};
-person.greet = function() {
-    return ` Hello, ${this.name}!`;
-};
-console.log(person.greet());
-
-// Question 3: How can you loop through the properties of an object?
-const user = {
-    name: "Tanveer",
-    age: 21,
-    location: "Ahmadnagar"
-};
-for(let key in user){
-    console.log(`${key}: ${user[key]}`);
-}
