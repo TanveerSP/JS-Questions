46ed5d07f97f8d483cef31ca6e0cc659edaedd8a
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-29 20:06:54 +0530

    Add a new property to an existing object and modify an existing property.   What happens if the object is declared with const?

---
+++ Objects/06_Add-new-prop-const.js
@@ -0,0 +1,10 @@
+//================================================================================ //
+// Add a new property to an existing object and modify an existing property.
+//  What happens if the object is declared with const?
+
+const book = {
+    title: "JavaScript Essentials",
+    author: "John Doe"
+};
+
+// Adding a new prp
