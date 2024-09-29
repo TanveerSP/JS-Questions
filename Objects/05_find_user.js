4bdd15099ca612ad981c16f67161d7de32f88939
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-29 20:02:07 +0530

    Add a new property to an existing object and modify an existing property. What happens if the object is declared with const?

--- Objects/05_find_user.js
+++ Objects/05_find_user.js
@@ -1,9 +1,24 @@
 // Question: Given an array of user objects,
 // find the user with a specific email.
 
-const userEmail = [
+const users = [
     { id: 1, email: "empAtMicrosoft@gmail.com" },
-    { id: 2, email: "empAtGoogle@gmail.com"},
-    { id: 3, email: "empAtAmazone@gmail.com"},
-    {id:4, email: "empAtNetflix@gmail.com"}
+    { id: 2, email: "empAtGoogle@gmail.com" },
+    { id: 3, email: "empAtAmazone@gmail.com" },
+    { id: 4, email: "empAtNetflix@gmail.com" }
 ]
+
+// Finding a user by email
+const targetEmail = "empAtMicrosoft@gmail.com";
+const foundUser = users.find(user => user.email === targetEmail);
+
+console.log(foundUser);
+// Output { id:1, email:'empAtMicrosoft@gmail.com'}
+
+const findEmail = "empAtGoogle@gmail.com";
+const prisentEmail = users.find(user=> user.email === targetEmail);
+
+//================================================================================ //
+// Add a new property to an existing object and modify an existing property.
+//  What happens if the object is declared with const?
+
