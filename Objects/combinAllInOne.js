c653fded2375c1b85b5edc2fe3cebf413397a051
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-30 11:51:01 +0530

    Let's combine everything you've learned into a small project where we define an object for a course, destructure its properties, add new properties, and use some methods like Object.keys() and Object.values() to manipulate and display the data.

---
+++ Objects/combinAllInOne.js
@@ -0,0 +1,4 @@
+/* Let's combine everything you've learned into a small project where we define an object for a course, destructure its properties,
+ add new properties, and use some methods like Object.keys() and Object.values() to manipulate and display the data. */
+
+ 
