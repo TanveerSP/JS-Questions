6fdf31ede611364ba3f404ebae1410e8fc39cc84
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-09-30 11:26:58 +0530

    Q2 If you want to destructure both coursename and price from the  course object, how would you do that?

--- Objects/08_destructure_Obj.js
+++ Objects/08_destructure_Obj.js
@@ -1,4 +1,4 @@
-// How would you destructure the coursename from the course 
+// Q1 How would you destructure the coursename from the course 
 // object and store it in a variable called name?
 
 const course = {
@@ -9,4 +9,12 @@ const course = {
 
 const {coursename: name} = course;
 console.log(name); // Output: "TS in Hindi"
+
+// Q2 If you want to destructure both coursename and price from the 
+// course object, how would you do that?
+
+const {coursename, price} = course;
+console.log(coursename);
+console.log(price);
+
 
