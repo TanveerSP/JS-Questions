96293aa1b9e6fb16e1440d8d2e9059fab0c9a336
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-10-02 12:02:12 +0530

    5Q qustion are complet

--- Function/03_Array_in_Fun.js
+++ Function/03_Array_in_Fun.js
@@ -1,11 +1,9 @@
 // Q1 Write a function findMaxNumber that accepts an array of numbers and returns the maximum number.
-
+//============================================================================================================================
 const numArr = [12, 23, 34, 45];
-
 function findMaxNumber(number) {
     return Math.max(...number)
 }
-
 // console.log(findMaxNumber(numArr)) // Output is: 45
 
 const newNumArr = [10, 20, 30, 40, 50];
@@ -14,8 +12,8 @@ function findMaxNum(arrNum) {
 }
 // console.log(findMaxNum(newNumArr));
 
+//============================================================================================================================
 // Q2 Create a function sumArray that accepts an array of numbers and returns the sum of all elements.
-
 const arr = [5, 10, 15];
 function sumArray(sumArr) {
     return sumArr.reduce((acc, curr) => acc + curr, 0) // Yeh line array ke saare elements ka sum return karti hai by using reduce(),
@@ -23,7 +21,8 @@ function sumArray(sumArr) {
 }
 // console.log(sumArray(arr));
 
-// Q4 Write a function findElementIndex that takes an array and an element, returning the index of the element in the array.
+//============================================================================================================================
+// Q3 Write a function findElementIndex that takes an array and an element, returning the index of the element in the array.
 const frutes = ['Apple', 'Banana', 'Mango', 'Bery'];
 function findElementIndex(arr, ele) {
     return arr.indexOf(ele);
@@ -34,6 +33,23 @@ function findCarInd(arr, ele) {
     return arr.indexOf(ele);
 }
 const cars = ['Mahendra', 'Tata', 'BMW', 'Ferare', 'Bugathi'];
-console.log(findCarInd(cars, 'Tata'));
-console.log(findCarInd(cars, 'Bugathi'));
-console.log(findCarInd(cars, 'Ferare'));
+// console.log(findCarInd(cars, 'Tata'));
+// console.log(findCarInd(cars, 'Bugathi'));
+// console.log(findCarInd(cars, 'Ferare'));
+
+//============================================================================================================================
+// Q4 Create a function arrayContains that takes an array and a value, and returns true if the value exists in the array, false otherwise.
+function isPrsentColor(arr, value) {
+    return arr.includes(value);
+}
+const color = ["Red", "Green", "Blue"];
+console.log(isPrsentColor(color, "Green"))
+console.log(isPrsentColor(color, "Yellow"));
+
+// =============================================================================================================================
+// Q5 Write a function getLastElement that returns the last element of an array.
+function getLastElement(numArr) {
+    return numArr[numArr.length - 1];
+}
+const num = [10, 11, 12, 13];
+console.log(getLastElement(num))
