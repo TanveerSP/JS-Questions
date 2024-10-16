db0e34d64b807ea5095b4566c1a401e9c5d246ca
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2024-10-16 19:55:50 +0530

    switch case 1 qus

---
+++ controles/02_switch.js
@@ -0,0 +1,34 @@
+// 1. Find the month name from a number
+
+function getMongthName(month) {
+    switch (month) {
+        case 1:
+            return 'jan';
+        case 2:
+            return 'feb';
+        case 3:
+            return 'mar';
+        case 4:
+            return 'apr';
+        case 5:
+            return 'may';
+        case 6:
+            return 'jun';
+        case 7:
+            return 'jul';
+        case 8:
+            return 'aug';
+        case 9:
+            return 'sep';
+        case 10:
+            return 'oct';
+        case 11:
+            return 'nov';
+        case 3:
+            return 'dec';
+        default:
+            return 'Invalid month number';
+    }
+}
+
+console.log(getMongthName(4))
