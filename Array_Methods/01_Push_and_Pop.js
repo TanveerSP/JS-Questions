31cb62b55b603625a46347ddaf709a37a99c92cb
Tanveer <tanveerpathan024@gmail.com>
2025-01-08 20:08:23 +0530

    changes

--- Array_Methods/01_Push_and_Pop.js
+++ Array_Methods/01_Push_and_Pop.js
@@ -1,22 +1,33 @@
-let frutes = [];
-console.log("Befor Array => ", frutes);
+// let frutes = [];
+// console.log("Befor Array => ", frutes);
 
-console.log(frutes.push("Apple", "Banana", "Chery", "Pinapple"));
-console.log("After Using Push Method => ", frutes);
+// console.log(frutes.push("Apple", "Banana", "Chery", "Pinapple"));
+// console.log("After Using Push Method => ", frutes);
 
-console.log(frutes.pop());
-console.log("After Using Pop Method => ", frutes);
+// console.log(frutes.pop());
+// console.log("After Using Pop Method => ", frutes);
 
-let carsCollections = [];
+// let carsCollections = [];
 
-carsCollections.push("Fortuner", "Supra","Ferari","Mustang")
-console.log("Cars Collections =>",carsCollections);
+// carsCollections.push("Fortuner", "Supra","Ferari","Mustang")
+// console.log("Cars Collections =>",carsCollections);
 
-carsCollections.pop();
-console.log("Delete Last Car =>",carsCollections);
+// carsCollections.pop();
+// console.log("Delete Last Car =>",carsCollections);
 
-let num = [];
-num.push(1,5,3,7,3,9,);
-let removedNum = num.pop();
-console.log(num);
-console.log(removedNum);
+// let num = [];
+// num.push(1,5,3,7,3,9,);
+// let removedNum = num.pop();
+// console.log(num);
+// console.log(removedNum);
+
+let greenFrutes = [];
+
+console.log("Befor Add Values in array => ", greenFrutes);
+greenFrutes.push("Green Apple", "Green Banana", "Green Orange");
+
+console.log("After add Values in Array => ", greenFrutes);
+
+// pop is an remove last element of array ;
+greenFrutes.pop();
+console.log(greenFrutes);
