6d5151d9a92cd0a50197d32fb82bc70807227d0e
Tanveer <tanveerpathan024@gmail.com>
2025-01-10 19:49:08 +0530

    charAt str method

--- 01_Basics/04_String.js
+++ 01_Basics/04_String.js
@@ -11,7 +11,7 @@ console.log(typeof name);
 
 // IMP String Methods :=>
 /*
-1) charAt()
+1) charAt()  
 2) concat()
 3) includes()
 4) indexOf()
@@ -20,5 +20,23 @@ console.log(typeof name);
 7) trim()
 8) split()
 9) replace() and replaceAll()
-10) substring(start, end)
+10) substring(start, end)       
 */
+
+console.log(name.length);
+console.log(name.toUpperCase());
+
+console.log(name.charAt(3));
+
+console.log(name.indexOf("r"));
+
+let gameName = "callOfDuty_Pubg";
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const newStr = gameName.slice(0, 8);
+console.log(newStr);
+
+const secStr = "   tanveer ";
+console.log(secStr); // print org string
+console.log(secStr.trim()); // remove extra spces 
---
+++ 02_String_Methods/charAt.js
@@ -0,0 +1,8 @@
+const owner = "Tanveer";
+console.log(owner.charAt(4));
+
+const userName = "Tanveer";
+const password = "userpassword";
+const phoneNo = 7744081343;
+
+console.log(userName.charAt(3), password.charAt(4));
