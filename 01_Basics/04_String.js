6c46897e3b24a07919f8557d43f67d9e0fbafd9e
Tanveer <tanveerpathan024@gmail.com>
2025-01-10 13:26:26 +0530

    add IMP methods in string

---
+++ 01_Basics/04_String.js
@@ -0,0 +1,24 @@
+const name = "tanveer";
+const repoCount = 50;
+
+// this is old type to concatenate string
+// console.log(name + repoCount + "value");
+
+// this is moder way to concatenate string
+console.log(`Hello my name is ${name} and my repo cout is ${repoCount}`);
+
+console.log(typeof name);
+
+// IMP String Methods :=>
+/*
+1) charAt()
+2) concat()
+3) includes()
+4) indexOf()
+5) slice()
+6) toLowerCase() and toUpperCase()
+7) trim()
+8) split()
+9) replace() and replaceAll()
+10) substring(start, end)
+*/
