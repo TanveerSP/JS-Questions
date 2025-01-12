ebf1e52fa9b349f5d891fdc12254267f2a3d0eca
Tanveer <tanveerpathan024@gmail.com>
2025-01-12 19:58:25 +0530

    we see How can you generate a random integer between two values, min and max?

---
+++ Math_Meth/random_num_btw_two_value.js
@@ -0,0 +1,8 @@
+// Q4 How do you round a number down to the nearest integer?
+console.log(Math.floor(4.335));
+console.log(Math.floor(5.999));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
