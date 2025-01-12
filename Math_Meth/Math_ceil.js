f203fe8d06aa8372783e11023528117dd2db0768
Tanveer <tanveerpathan024@gmail.com>
2025-01-12 21:35:15 +0530

    practice more qus

--- Math_Meth/Math_ceil.js
+++ Math_Meth/Math_ceil.js
@@ -1,5 +1,21 @@
+// Q1  What function would you use to round a number up to the next integer?
+// console.log(Math.ceil(8.4));
+// console.log(Math.ceil(-3.4));
 
-// Q3  What function would you use to round a number up to the next integer?
-console.log(Math.ceil(8.4));
-console.log(Math.ceil(-3.4));
+// Q2 
+console.log(Math.ceil(10));
 
+// Q3 
+console.log(Math.ceil(4.7));
+
+// Q4 
+const number = [1,2,3,4,5,6,7,8,9] 
+const result = number.map(Math.ceil);
+console.log(result);
+
+// Q5 
+
+function sumCeilValues(arr) {
+    return arr.reduce((sum, num) => sum + Math.ceil(num), 0);
+}
+console.log(sumCeilValues([2.3,3.8,1.1]));
