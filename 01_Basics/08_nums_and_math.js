b82391a7ccd2a065cb65128d360c8306dc37be1d
Tanveer <tanveerpathan024@gmail.com>
2025-01-12 19:28:01 +0530

    we see Math in js. How to work math? and  why this important and see Math() functions

--- 01_Basics/08_nums_and_math.js
+++ 01_Basics/08_nums_and_math.js
@@ -1,4 +1,41 @@
 const score = 400;
+// console.log(score);
 
-const balance = new Number(100)
-console.log(balance)
+const balance = new Number(100);
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
+
+const otherNum = 23.9898;
+// console.log(otherNum.toPrecision(3));
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-In'));
+
+// ================ Math ================= //
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(5.79));
+// console.log(Math.ceil(4.2));
+
+// console.log(Math.floor(4.9));
+
+// console.log(Math.sqrt(4));
+
+// console.log(Math.min(1,2,3,4,6,7,8));
+// console.log(Math.max(1,2,3,4,6,7,8))
+
+console.log(Math.random()); // this give '0' or '1'
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 0) + 1); // this give single deget number
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+
