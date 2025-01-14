4ae6364c090c134433018fbe307f8ff940feb456
Tanveer <tanveerpathan024@gmail.com>
2025-01-14 20:16:57 +0530

    code

---
+++ 02_Basics/02_fun_and_obj_arr.js
@@ -0,0 +1,28 @@
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(200, 500, 400))
+
+const user = {
+  username: "tanveer",
+  price: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+// handleObject(user)
+handleObject({
+  username: "sam{",
+  price: 399,
+});
+
+const myNewArra = [200, 300, 400];
+
+function retrunSecondValue(getArray) {
+  return getArray[1];
+}
+console.log(retrunSecondValue(myNewArra));
