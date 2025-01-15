15c4f9b96fc31dca741aeb90739b9b4d17ed7911
Tanveer <tanveerpathan024@gmail.com>
2025-01-15 14:58:02 +0530

    we learn in this repository scope level and mini hoisting

--- 02_Basics/03_scope.js
+++ 02_Basics/03_scope.js
@@ -9,10 +9,51 @@ if (true) {
   let a = 10;
   const b = 20;
   //   var c = 30
-  console.log("Inner: Block Scope a",a)
+  // console.log("Inner: Block Scope a",a)
 }
 
-console.log("Outer: Global Scope a",a);
-console.log(b);
-console.log(c);
+// console.log("Outer: Global Scope a",a);
+// console.log(b);
+// console.log(c);
+
+
+
+function one() {
+  const username = "tanveer";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website);
+
+  two();
+}
+
+// one();
+
+if (true) {
+  const username = "veer";
+
+  if (username === "veer") {
+    const website = " youtube";
+    // console.log(username + website);
+  }
+
+  // console.log(website);
+}
+// console.log(username);
+
+// *****************  interesting  *********************
+
+console.log(addone(5));
+function addone(num) {
+  return num + 1;
+}
+
+addTwo(5);
+// this fun is hoisting
+const addTwo = function (num) {
+  return num + 2;
+};
 
