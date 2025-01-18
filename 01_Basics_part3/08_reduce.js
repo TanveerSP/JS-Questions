527a6d2903020ab7abe369ab9ba9a7a130a6486b
Tanveer <tanveerpathan024@gmail.com>
2025-01-18 11:58:55 +0530

    i learn about what is reduce and how work

--- 01_Basics_part3/08_reduce.js
+++ 01_Basics_part3/08_reduce.js
@@ -1,8 +1,32 @@
 const myNums = [1, 2, 3];
 
-const myTotal = myNums.reduce(function (acc, currval) {
-  console.log(`acc :${acc} and currval ${currval}`);
-  return acc + currval;
-}, 3);
+// const myTotal = myNums.reduce(function (acc, currval) {
+//   console.log(`acc :${acc} and currval ${currval}`);
+//   return acc + currval;
+// }, 0);
+
+const myTotal = myNums.reduce((accu, currentValu) => accu + currentValu, 0);
 
 console.log(myTotal);
+
+const shoppinCart = [
+  {
+    itemName: "js coutse",
+    price: 2999,
+  },
+  {
+    itemName: "python coutse",
+    price: 999,
+  },
+  {
+    itemName: "app coutse",
+    price: 5999,
+  },
+  {
+    itemName: "data science coutse",
+    price: 12999,
+  },
+];
+
+const priceToPay = shoppinCart.reduce((accu, item) => accu + item.price, 0);
+console.log(priceToPay)
