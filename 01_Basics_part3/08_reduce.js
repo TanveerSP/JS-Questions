dfd878174a2ef29b9a26ef6c130be342f183e9a1
Tanveer <tanveerpathan024@gmail.com>
2025-01-18 10:29:28 +0530

    this file i learn reduce

--- 01_Basics_part3/08_reduce.js
+++ 01_Basics_part3/08_reduce.js
@@ -1,7 +1,8 @@
 const myNums = [1, 2, 3];
 
 const myTotal = myNums.reduce(function (acc, currval) {
-    return acc + currval
-}, 0)
+  console.log(`acc :${acc} and currval ${currval}`);
+  return acc + currval;
+}, 3);
 
 console.log(myTotal);
