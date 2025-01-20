4694038cbe170d891c9d860170b03bbf656d634d
Tanveer <tanveerpathan024@gmail.com>
2025-01-20 18:22:21 +0530

    practice more how to create element and how modify and show in html code

--- 04_Dom/03_Three.js
+++ 04_Dom/03_Three.js
@@ -1,13 +1,79 @@
+// const div = document.createElement("div");
+// console.log(div);
+// div.className = "main";
+// // div.di = round(Math.random() * 10 + 1);
+// div.setAttribute("title", "gernerted title");
+// div.style.backgroundColor = "darkgreen";
+// div.style.padding = "12px";
+// div.style.color = "white";
+
+// // div.innerText = "Learn about hot to create new element";
+// const addText = document.createTextNode("Learn about hot to create new element");
+
+// div.appendChild(addText);
+
+// document.body.appendChild(div)
+
+// How to create element in HTML using JS
 const div = document.createElement("div");
 console.log(div);
-div.className = "main";
-// div.di = round(Math.random() * 10 + 1);
-div.setAttribute("title", "gernerted title");
-div.style.backgroundColor = "darkgreen";
-div.style.padding = "12px";
-div.style.color = "white";
-// div.innerText = "Learn about hot to create new element";
-const addText = document.createTextNode("Learn about hot to create new element");
-div.appendChild(addText);
 
-document.body.appendChild(div)
+// How to give class name to div in js
+div.className = "main";
+console.log("div class :-", div.outerHTML);
+
+// How to give ID for div
+div.id = "parent";
+console.log("div Id:-", div.outerHTML);
+
+// How to give random name to div in js
+// div.id.round(Math.random() * 10 + 1);
+
+// How to give set attribute in js
+div.setAttribute("title", "genreted title");
+console.log("div title :-", div.outerHTML);
+
+// How to style created elem 'div'
+div.style.backgroundColor = "green";
+div.style.color = "white";
+div.style.padding = "18px";
+
+// How to add text or content inside of div
+const addText = document.createTextNode("Hey, I'am tanveer");
+
+// How to show content on browser
+div.appendChild(addText);
+document.body.appendChild(div);
+
+// Create h1 element
+const h1 = document.createElement("h1");
+console.log(h1);
+
+h1.style.color = "White";
+h1.style.backgroundColor = "darkgreen";
+h1.style.padding = "18px";
+
+const h1Text = document.createTextNode("Hi I am Full/MERN Stack Developer");
+h1.appendChild(h1Text);
+
+document.body.append(h1);
+
+const p1 = document.createElement("p1");
+
+let p1Text = document.createTextNode(
+  "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, quidem."
+);
+p1.style.backgroundColor = "green"
+p1.style.padding = "20px";
+p1.style.color = "white"
+p1.appendChild(p1Text);
+document.body.appendChild(p1);
+
+const h2 = document.createElement('h2');
+let h2Text = document.createTextNode("Passion, Hard Work, Dadecation, Strong Mindset, Big Gols")
+h2.appendChild(h2Text)
+document.body.appendChild(h2);
+h2.style.backgroundColor = "white"
+h2.style.padding = "20px"
+h2.style.color = "darkgreen"
+h2.style.margin = "20px"
