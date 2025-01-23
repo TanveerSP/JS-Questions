4846b3d9b0fb4126c8379ec57b5842fde914b5e8
Tanveer <tanveerpathan024@gmail.com>
2025-01-23 15:50:44 +0530

    re code solution

--- Project_03/script.js
+++ Project_03/script.js
@@ -1,9 +1,27 @@
-const clock = document.getElementById("clock");
-// const clock = document.querySelector('#clock')
+// const clock = document.getElementById("clock");
+// // const clock = document.querySelector('#clock')
 
-//
-setInterval(function () {
-  let date = new Date();
-  //   console.log(date.toLocaleString());
-  clock.innerHTML = date.toLocaleTimeString();
-}, 1000);
+// //
+// setInterval(function () {
+//   let date = new Date();
+//   //   console.log(date.toLocaleString());
+//   clock.innerHTML = date.toLocaleTimeString();
+// }, 1000);
+
+
+// const clock = document.getElementById("clock");
+
+// setInterval(function () {
+//   let date = new Date()
+//   // console.log(date.toLocaleDateString());
+//   clock.innerHTML = date.toLocaleTimeString();
+// },1000)
+
+const timeClock = document.querySelector('#clock');
+
+setInterval( () => {
+  let date = new Date()
+  // timeClock.innerHTML = date.toLocaleDateString();
+  // timeClock.innerHTML = date.toLocaleTimeString();
+  timeClock.innerHTML = date.toLocaleString();
+},1000)
