cd5df056dda08e7143eda50052875e992c8b9fbe
Tanveer <tanveerpathan024@gmail.com>
2025-01-27 19:31:08 +0530

    constructro function

--- 10_OOP/oop.js
+++ 10_OOP/oop.js
@@ -1,13 +1,27 @@
 // Object letterl
 
 const user = {
-    username: "tanveer",
-    loginCount: 8,
-    signedIn: true,
+  username: "tanveer",
+  loginCount: 8,
+  signedIn: true,
 
-    getUserDetails: () => {
-        console.log("hello");
-    }
-}
-console.log(user.username);
-console.log(user.getUserDetails);
+  getUserDetails: function () {
+    // console.log("hello");
+    // console.log(`Username: ${this.username}`);
+    console.log(this);
+  },
+};
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+
+//  =================================== //
+// constructor functions
+//  =================================== //
+
+// 'new' is a constructor function 
+// 'new' keyword use to create new context for there variable
+const promiseOne = new Promise()
+const date = new Date()
+
