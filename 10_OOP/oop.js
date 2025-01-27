21a72a79edb2e138b59497108ac53708663ba952
Tanveer <tanveerpathan024@gmail.com>
2025-01-27 20:00:14 +0530

    i learn about importance of 'new' keyword

--- 10_OOP/oop.js
+++ 10_OOP/oop.js
@@ -15,13 +15,33 @@ const user = {
 // console.log(user.getUserDetails());
 // console.log(this);
 
-
 //  =================================== //
 // constructor functions
 //  =================================== //
 
-// 'new' is a constructor function 
+// 'new' is a constructor function
 // 'new' keyword use to create new context for there variable
-const promiseOne = new Promise()
-const date = new Date()
+// const promiseOne = new Promise()
+// const date = new Date()
+
+function User(username, loginCount, isLoggedIn) {
+    // leftside username is a variable and rightside username is 'passed in function' 
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    // return this // if we dont defi return this 
+    // this is implicetly defind
+}
+
+// if we call function like that then function is "overide" that way use "new" keyword
+// const userOne = User("tanveer",12,true);
+// const userTwo = User("CodeWithveer",11,false)
+// console.log(userOne);
+
+// use new keyword
+const userOne = new User("tanveer",12,true);
+const userTwo = new User("CodeWithveer",11,false) 
+// console.log(userOne);
+// console.log(userTwo);
 
