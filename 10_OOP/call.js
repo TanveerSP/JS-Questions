68c9ba6ffe10b51008cab0f204b76efa2299fe62
Tanveer <tanveerpathan024@gmail.com>
2025-01-28 16:11:18 +0530

    practice on call

--- 10_OOP/call.js
+++ 10_OOP/call.js
@@ -1,15 +1,30 @@
-function SetUsername(username) {
-  // complex DB calls
+// function SetUsername(username) {
+//   // complex DB calls
+//   this.username = username;
+//   console.log("called");
+// }
+
+// function createUser(username, email, passwordd) {
+//   SetUsername.call(this, username);
+
+//   this.email = email;
+//   this.passwordd = passwordd;
+// }
+
+// const chai = new createUser("tanveer", "tanveer@gmail", "123");
+// console.log(chai);
+
+function Setusername(username) {
   this.username = username;
   console.log("called");
 }
-
-function createUser(username, email, passwordd) {
-  SetUsername.call(this, username);
+function createUser(username, email, password) {
+  Setusername.call(this, username);
 
   this.email = email;
-  this.passwordd = passwordd;
+  this.password = password;
 }
 
-const chai = new createUser("tanveer", "tanveer@gmail", "123");
+//
+const chai = new createUser("tanveer", "tanveer@openAigmail.com", "222");
 console.log(chai);
