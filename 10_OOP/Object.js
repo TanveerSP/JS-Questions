748bc9fa924229a9b5baf3997870eacb3a62f584
Tanveer <tanveerpathan024@gmail.com>
2025-01-28 12:39:18 +0530

    Prototype in javascript

---
+++ 10_OOP/Object.js
@@ -0,0 +1,8 @@
+function mulitipleyBy5(num) {
+    return num * 5
+}
+mulitipleyBy5.power=2;
+
+console.log(mulitipleyBy5(5));
+console.log(mulitipleyBy5.power);
+console.log(mulitipleyBy5.prototype);
