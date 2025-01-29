586c70ea6ba5ebfb6ab5490c2834256050a860a6
Tanveer <tanveerpathan024@gmail.com>
2025-01-29 16:56:33 +0530

    If we change Math.PI value if no then why

--- 10Classes_OOP/mathpi.js
+++ 10Classes_OOP/mathpi.js
@@ -11,14 +11,30 @@ const chai = {
   name: "ginger chai",
   price: 250,
   isAvailable: true,
+
+  orderChai: function () {
+    console.log("chai nahi bane");
+  },
 };
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 // console.log(Object.getOwnPropertyDescriptor(chai, 'price'));
 // console.log(Object.getOwnPropertyDescriptor(chai, 'isAvailable'));
 
 Object.defineProperty(chai, "name", {
-  writable: false,
+//   writable: false,
   enumerable: false,
 });
 
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+// if we try to access inside value of this Obj then he gives error this is not itreable
+// for (let [key, value] of chai){
+//     console.log(`${key} : ${value}`);
+// }
+
+//  How to access ? access like that
+for (let [key, value] of Object.entries(chai)) {
+  if (typeof value !== "function") {
+    console.log(`${key} : ${value}`);
+  }
+}
